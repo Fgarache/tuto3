0fdee5765767951135e40e9bc6840e332528f6ce
Francisco Garache <franciscogarach3@gmail.com>
2022-05-13 18:21:45 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,29 @@
+
+
+let age = 31
+let min = UInt8.min //2^0- = 0
+let max = UInt8.max //2^8-1 = 251
+
+let f1: Float =  3.143545334 //solo permite 6 desimales
+let d1: Double = 3.142345455
+
+
+let decimal = 17 //1*10+7 =17
+let numeroBinario = 0b10001 // 17
+let octalNumber = 0o21 //17
+let hexadecimal = 0x11 // 17
+
+let decimlaDouble = 12.1875 
+let exponentDouble = 1.21875e1 //decimlaDouble = 12.1875 
+let paddend = 000000000123.456 //123.456
+let oneMillion = 1_000_000 //1 millon
+let moreOneMillion = 1_000_000.000_000_1 //1 millon
+
+
+
+print(numeroBinario)
+print(octalNumber)
+print(hexadecimal)
+print(exponentDouble)
+print(paddend)
+print (oneMillion)
